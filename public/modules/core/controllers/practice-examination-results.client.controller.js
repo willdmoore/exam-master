67dc45b6a77048d8edcfb418651bdda4d6165e07
Bill Moore <moore.william.d@gmail.com>
2015-07-07 21:42:17 -0400

    More changes for user dashboard.

---
+++ public/modules/core/controllers/practice-examination-results.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').controller('PracticeExamininationResultsController', ['$scope',
+	function($scope) {
+		// Practice examinination results controller logic
+		// ...
+	}
+]);
